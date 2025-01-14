4e0322d255ac5c3db85bb65baba0bae377aab740
mn-steady <mn-steady@users.noreply.github.com>
2025-01-14 23:45:21 +0000

    deploy: dd06a58e7f428c70dff5909dee25f8d15967008d

---
+++ pkg/yolonode_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __wbindgen_export_0: (a: number) => void;
+export const __wbindgen_export_1: (a: number, b: number) => number;
+export const __wbindgen_export_2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_export_4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
